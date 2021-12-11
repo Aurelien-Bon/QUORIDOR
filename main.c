2fa8c52402bf54ee917828aeb205e3b6eea0c8a9
alexbelbo <alexandre.bellard@edu.ece.fr>
2021-12-11 20:33:58 +0100

    version 0.11.6
    affichage des regles du jeu

--- main.c
+++ main.c
@@ -9,13 +9,59 @@ int main()
     menu();
     return 0;
 }
-/*
+
 void regleJeu()
 {
-    printf("Voici les règles du jeu :\n");
-    printf("")
+    int quitter = 0;
+    char c;
+    c=toucheAppuiez();
+    while (quitter == 0)
+    {
+        printf("   ______________________________________________________________________         \n");
+        printf("  |                                                                      |        \n");
+        printf("  |                      +-+-+-+-+-+-+ +-+-+ +-+-+-+                     |        \n");
+        printf("  |                      |R|E|G|L|E|S| |D|U| |J|E|U|                     |        \n");
+        printf("  |                      +-+-+-+-+-+-+ +-+-+ +-+-+-+                     |        \n");
+        printf("  |______________________________________________________________________|        \n");
+        printf("  |                                                                      |        \n");
+        printf("  |                          ( - BUT DU JEU - )                          |        \n");
+        printf("  |                                                                      |        \n");
+        printf("  |             Deux modes de jeu possibles : 2 ou 4 joueurs.            |        \n");
+        printf("  |  Etre le premier a atteindre la ligne opposee a sa ligne de depart.  |        \n");
+        printf("  |     Un tirage au sors de facon aleatoire determine qui commence.     |        \n");
+        printf("  |    Les barrieres sont reparties equitablement entre les joueurs.     |        \n");
+        printf("  |        Pour 2 joueurs : chaque joueur dispose de 10 barrieres        |        \n");
+        printf("  |        Pour 4 joueurs : chaque joueur dispose de 5 barrieres         |        \n");
+        printf("  |                                                                      |        \n");
+        printf("  |                                                                      |        \n");
+        printf("  |                         ( - DEPLACEMENTS - )                         |        \n");
+        printf("  |                                                                      |        \n");
+        printf("  |                A tour de role chaque joueur choisi de :              |        \n");
+        printf("  |  1) Deplacer son pion d'une case (Vertical/Horizontal/Avant/Arriere) |        \n");
+        printf("  |  2) Poser une de ses barrieres. Une barriere doit etre posee entre 2 |        \n");
+        printf("  |   blocs de 2 cases. Lorsqu'il n'a plus de barrieres, un joueur est   |        \n");
+        printf("  |                     oblige de deplacer son pion.                     |        \n");
+        printf("  |  => Objectif : creer son propre chemin ou ralentir son adversaire    |        \n");
+        printf("  |                                                                      |        \n");
+        printf("  |                        ( - SPECIFICATIONS - )                        |        \n");
+        printf("  |                                                                      |        \n");
+        printf("  |         /!\\  Interdiction de fermer totalement l'acces  /!\\          |      \n");
+        printf("  |  Si 2 pions se retrouvent face a face le joueur a le droit de sauter |        \n");
+        printf("  |                      par dessus sont adversaire.                     |        \n");
+        printf("  |  Si une barriere se trouve derriere le pion saute, le joueur peut    |        \n");
+        printf("  |       choisir de bifurquer a droite ou a gauche du pion saute.       |        \n");
+        printf("  |______________________________________________________________________|      \n\n");
+        Color(12,0);
+        printf("                 APPUYER SUR LA TOUCHE ENTREE POUR QUITTER                        \n");
+        scanf("%c", &c);
+        Color(15,0);
+        if(c==' ')
+            {
+                quitter=1;
+            }
+    }
 }
-*/
+
 /*
 void scoreJoueur()
 {
@@ -40,7 +86,7 @@ void menu()
     int finpartie = 0;
     while(finpartie == 0)
     {
-       int choix=1;
+        int choix=1;
         int quitter=0;
         char c;
         while(quitter==0)
@@ -59,7 +105,7 @@ void menu()
             printf("_____________\\/////_\\///________\\/////////____________\\/////_________\\///________\\///____\\///////////____\\////////////____________\\/////_________\\///________\\///____________\n");
             gotoligcol(20,0);
             Color(15,0);
-            printf("                                                                    _________________________________________        \n");
+            printf("                                                                    _________________________________________         \n");
             printf("                                                                   /                                         \\       \n");
             printf("                                                                  /       1. LANCER UNE NOUVELLE PARTIE       \\      \n");
             printf("                                                                 /     2. REPRENDRE UNE PARTIE SAUVEGARDEE     \\     \n");
@@ -100,8 +146,8 @@ void menu()
                 break;
 
             case 3 :
-
-            break;
+                regleJeu();
+                break;
 
             case 4 :
                 //scoreJoueur();
